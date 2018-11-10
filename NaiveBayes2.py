58abcc67938e4321ac7ed5cdd741f02bec770e79
Rendson.Fernandes <rendson.fernandes@gmail.com>
2018-11-09 22:44:38 -0300

    Create File

---
+++ NaiveBayes2.py
@@ -0,0 +1,28 @@
+import csv
+import random
+
+def loadCsv(filename):
+    lines = csv.reader(open(filename))
+    # csv.reader(open(filename, "rb"))
+    dataset = list(lines)
+    for i in range(len(dataset)):
+        dataset[i] = [float(x) for x in dataset[i]]
+    return dataset
+
+def splitDataset(dataset, splitRatio):
+    trainSize = int(len(dataset) * splitRatio)
+    trainSet = []
+    copy = list(dataset)
+    while len(trainSet) < trainSize:
+        index = random.randrange(len(copy))
+        trainSet.append(copy.pop(index))
+    return [trainSet, copy]
+
+filename = 'pima-indians-diabetes.data.csv'
+dataset = loadCsv(filename)
+
+dataset = [[1], [2], [3], [4], [5]]
+splitRatio = 0.67
+train, test = splitDataset(dataset, splitRatio)
+
+print(len(dataset), train, test)
